1759e1830b7f5780e867d5a5ec8f19d0049f87a3
pankajfapl <pankaj.pandey@floweraura.com>
2025-08-05 08:22:00 +0530

    user apis4

--- routes/api.php
+++ routes/api.php
@@ -53,7 +53,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
 
 
-Route::middleware(['throttle:10,1', 'cors'])->group(function () {
+Route::middleware(['throttle:10,1'])->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login'])->name('login');
     // Route::options('/register', function () {
