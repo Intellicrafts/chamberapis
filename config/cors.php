6bbdbb0a7759e99b256d335cec1ce93293f19e26
Devesh <Devesh.yadav@floweraura.com>
2025-08-16 19:08:26 +0530

    cors added for mderabakil

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['capacitor://localhost', 'http://localhost', 'http://localhost:8100', 'http://localhost:5173', 'https://intellicrafts.in', 'http://localhost:3000', 'http://127.0.0.1:3000'],
+    'allowed_origins' => ['capacitor://localhost', 'http://localhost', 'http://localhost:8100', 'http://localhost:5173', 'https://intellicrafts.in','https://merabakil.com/','http://localhost:3000', 'http://127.0.0.1:3000'],
 
     'allowed_origins_patterns' => [],
 
