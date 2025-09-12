75f577d61af177f0b9d8a304d71fa06b1345007b
Devesh <Devesh.yadav@floweraura.com>
2025-09-12 16:23:06 +0530

    password validation issue fixed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -37,7 +37,7 @@ class AuthController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255|regex:/^[a-zA-Z\s]+$/',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/',
+            'password' => 'required|string|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]/',
             'account_type' => 'nullable',
             'license_number' => 'required_if:account_type,business,lawyer|string|max:50',
             'specialization' => 'required_if:account_type,business,lawyer|string|max:255',
